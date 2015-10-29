62b4359d36268c46e18d71d82663cf5261791949
Dave Rooney <daverooney@users.noreply.github.com>
2015-10-28 20:37:36 -0400

    Update Candidate-counters.py

--- Candidate-counters.py
+++ Candidate-counters.py
@@ -6,9 +6,17 @@ from collections import Counter
 auth.set_access_token(ACCESSTOKEN, ACCESSSECRET)
 api = tweepy.API(auth)
 
-CandidateList = ["Trump", "Carson", "Bush", "Rand Paul", "Kasich", "Fiorina", "Rubio", "
+stopwords = ["a", "the", "is", "I", "to", "of", "that", "and"
+CandidateList = ["Trump", "Carson", "Bush", "Rand Paul", "Kasich", "Fiorina", "Rubio", "Huckabee", "Cruz", "Christie"]
+CandCntrs = {}
+for c in CandidateList:
+  CandCntrs[c] = Counter()
 
 try:
   while 1:
-  
+    for cand in CandidateList:
+        tweets = api.search(cand)
+          for t in tweets:
+            for word in t:
+              if 
 api.search('Trump')
