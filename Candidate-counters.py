ae081623fa6426e332a3e0cba224d08933ca6c02
Dave Rooney <daverooney@users.noreply.github.com>
2015-10-28 20:27:51 -0400

    Create Candidate-counters.py

---
+++ Candidate-counters.py
@@ -0,0 +1,14 @@
+import tweepy
+from tweepy import OAuthHandler
+from TwitterKeys import *
+from collections import Counter
+
+auth.set_access_token(ACCESSTOKEN, ACCESSSECRET)
+api = tweepy.API(auth)
+
+CandidateList = ["Trump", "Carson", "Bush", "Rand Paul", "Kasich", "Fiorina", "Rubio", "
+
+try:
+  while 1:
+  
+api.search('Trump')
