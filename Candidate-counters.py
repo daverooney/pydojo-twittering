69628af2dc18ddb3fcc9552dd7ade91560c15ae9
Dave Rooney <daverooney@users.noreply.github.com>
2015-10-29 00:25:19 -0400

    Update Candidate-counters.py

--- Candidate-counters.py
+++ Candidate-counters.py
@@ -8,7 +8,7 @@ auth = tweepy.OAuthHandler(APIKEY, APISECRET)
 auth.set_access_token(ACCESSTOKEN, ACCESSSECRET)
 api = tweepy.API(auth)
 
-stopwords = ["a", "the", "is", "I", "to", "of", "that", "and", "RT", "rt", "in", "Mr.", "Ms.", "Mrs."]
+stopwords = ["a", "the", "is", "I", "to", "of", "that", "and", "RT", "rt", "in", "Mr.", "Ms.", "Mrs.", "on", "for", "was", "be", "am"]
 CandidateList = ["Trump", "Carson", "Bush", "Rand Paul", "Kasich", "Fiorina", "Rubio", "Huckabee", "Cruz", "Christie"]
 CandCounters = {}
 for c in CandidateList:
@@ -25,7 +25,7 @@ try:
       time.sleep(6)
 #      CandCounters[cand] = CandCounters[cand].most_common(5000)
       pprint(CandCounters[cand].most_common(6))
-except:
+except KeyboardInterrupt:
   for cand in CandidateList:
     pprint(CandCounters[cand].most_common(10))
   
